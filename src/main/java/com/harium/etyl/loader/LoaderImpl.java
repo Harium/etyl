deafdc8ef52eabcab9139025a628967bc8ded029
Yuri Pourre <yuripourre@gmail.com>
2017-10-27 15:25:14 -0200

    Improves remote image loading

--- src/main/java/com/harium/etyl/loader/LoaderImpl.java
+++ src/main/java/com/harium/etyl/loader/LoaderImpl.java
@@ -1,69 +1,80 @@
 package com.harium.etyl.loader;
 
+import com.harium.etyl.util.io.IOHelper;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import com.harium.etyl.util.io.IOHelper;
-
 /**
- * 
  * @author yuripourre
- *
  */
 
 public class LoaderImpl implements Loader {
 
-	protected URL url;
+    protected URL url;
 
-	protected String folder;
-	
-	public void setUrl(String s) {
-		try {
-			url = new URL(s);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-	} 
+    protected String folder;
 
-	public URL getUrl() {
-		return url;
-	}
-	
-	public URL getFullURL(String filename) throws MalformedURLException {
-		return new URL(url, folder+filename);
-	}
-	
-	public String getPath() {
-		
-		String path = url.toString();
-		path = path.substring(IOHelper.FILE_PREFIX.length());
-		path = path.replaceAll("%20", " ");
-		
-		return path;
-	}
-	
-	@Override
-	public void initLoader() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public String fullPath(String path) {
-		return fullPath(path, false);
-	}
-	
-	public String fullPath(String path, boolean absolute) {
+    public void setUrl(String s) {
+        try {
+            url = new URL(s);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
 
-		StringBuilder sb = new StringBuilder();
+    public URL getUrl() {
+        return url;
+    }
 
-		if(!absolute) {
-			sb.append(url.getPath());
-			sb.append(folder);
-		}
+    public URL getFullURL(String filename) throws MalformedURLException {
+        return new URL(url, folder + filename);
+    }
 
-		sb.append(path);
+    public String getPath() {
+        String path = url.toString();
 
-		return sb.toString();
-	}
-	
+        if (path.startsWith(IOHelper.FILE_PREFIX)) {
+            path = path.substring(IOHelper.FILE_PREFIX.length());
+            path = path.replaceAll("%20", " ");
+        }
+
+        return path;
+    }
+
+    @Override
+    public void initLoader() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public String fullPath(String path) {
+        return fullPath(path, false);
+    }
+
+    public String fullPath(String path, boolean absolute) {
+        StringBuilder sb = new StringBuilder();
+
+        if (isHttpContext()) {
+            sb.append(getPath());
+            sb.append(folder);
+            sb.append(path);
+            return sb.toString();
+        }
+
+        if (!absolute) {
+            sb.append(url.getPath());
+            sb.append(folder);
+        }
+
+        sb.append(path);
+
+        return sb.toString();
+    }
+
+    protected boolean isHttpContext() {
+        return IOHelper.startsWithHttp(getPath());
+    }
+
+
 }
