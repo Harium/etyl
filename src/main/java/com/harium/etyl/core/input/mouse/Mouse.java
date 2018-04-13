226e3405b6ccf7fb8f0f19bcdc27aa3bbedc84ae
Yuri Pourre <yuripourre@gmail.com>
2018-04-13 11:59:56 -0300

    Improves Mouse and allows multitouch

--- src/main/java/com/harium/etyl/core/input/mouse/Mouse.java
+++ src/main/java/com/harium/etyl/core/input/mouse/Mouse.java
@@ -1,230 +1,210 @@
 package com.harium.etyl.core.input.mouse;
 
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
-
-import javax.swing.event.MouseInputListener;
-
 import com.harium.etyl.commons.event.MouseEvent;
 import com.harium.etyl.commons.event.PointerEvent;
 import com.harium.etyl.commons.event.PointerState;
-import com.harium.etyl.util.concurrency.ConcurrentList;
+
+import javax.swing.event.MouseInputListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.util.*;
 
 /**
- * 
  * @author yuripourre
- *
  */
 
 public class Mouse implements MouseMotionListener, MouseInputListener, MouseWheelListener {
 
-	protected int x = 0;
-	protected int y = 0;
-	protected int z = 0;
+    protected int x = 0;
+    protected int y = 0;
+    protected int z = 0;
 
-	protected int clicks = 0;
+    // Allows mutiple cursors / multi-touch
+    Map<Integer, DragEvent> dragged = new HashMap<>();
+    Deque<PointerEvent> events = new ArrayDeque<>(8);
 
-	protected boolean dragged = false;
+    public Mouse(int x, int y) {
+        super();
 
-	protected int dragButton = 0;
+        this.x = x;
+        this.y = y;
 
-	protected int dragX = 0;
-	protected int dragY = 0;
+        dragged.put(java.awt.event.MouseEvent.BUTTON1, new DragEvent());
+        dragged.put(java.awt.event.MouseEvent.BUTTON2, new DragEvent());
+        dragged.put(java.awt.event.MouseEvent.BUTTON3, new DragEvent());
+    }
 
-	private Deque<PointerEvent> events = new ArrayDeque<>(8);
-	
-	public Mouse(int x, int y) {
-		super();
+    public int getX() {
+        return x;
+    }
 
-		this.x = x;
-		this.y = y;
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	public int getX() {
-		return x;
-	}
+    public int getY() {
+        return y;
+    }
 
-	public void setX(int x) {
-		this.x = x;
-	}
+    public void setY(int y) {
+        this.y = y;
+    }
 
-	public int getY() {
-		return y;
-	}
+    public int getZ() {
+        return z;
+    }
 
-	public void setY(int y) {
-		this.y = y;
-	}
+    public void setZ(int z) {
+        this.z = z;
+    }
 
-	public int getZ() {
-		return z;
-	}
+    public void setCoordinates(int x, int y) {
+        setX(x);
+        setY(y);
+    }
 
-	public void setZ(int z) {
-		this.z = z;
-	}
+    private void addEvent(int button, PointerState state) {
+        addEvent(button, state, 0);
+    }
 
-	public void setCoordinates(int x, int y) {
-		setX(x);
-		setY(y);
-	}
+    private void addEvent(int button, PointerState state, int amount) {
+        addEvent(button, state, 0, amount);
+    }
 
-	private void addEvent(int button, PointerState state) {
-		addEvent(button, state, 0);
-	}
+    private void addEvent(int button, PointerState state, int amountX, int amountY) {
+        MouseEvent key = MouseEvent.MOUSE_NONE;
 
-	private void addEvent(int button, PointerState state, int amount) {
-		addEvent(button, state, 0, amount);
-	}
+        switch (button) {
+            case java.awt.event.MouseEvent.BUTTON1:
+                key = MouseEvent.MOUSE_BUTTON_LEFT;
+                break;
+            case java.awt.event.MouseEvent.BUTTON2:
+                key = MouseEvent.MOUSE_BUTTON_MIDDLE;
+                break;
+            case java.awt.event.MouseEvent.BUTTON3:
+                key = MouseEvent.MOUSE_BUTTON_RIGHT;
+                break;
+        }
 
-	private void addEvent(int button, PointerState state, int amountX, int amountY) {
+        events.add(new PointerEvent(key, state, x, y, amountX, amountY));
+    }
 
-		MouseEvent key = MouseEvent.MOUSE_NONE;
+    @Override
+    public void mouseClicked(java.awt.event.MouseEvent me) {
 
-		switch (button) {
-		case java.awt.event.MouseEvent.BUTTON1:
-			key = MouseEvent.MOUSE_BUTTON_LEFT;
-			break;
-		case java.awt.event.MouseEvent.BUTTON2:
-			key = MouseEvent.MOUSE_BUTTON_MIDDLE;
-			break;
-		case java.awt.event.MouseEvent.BUTTON3:
-			key = MouseEvent.MOUSE_BUTTON_RIGHT;
-			break;
-		}
+        PointerState state = PointerState.CLICK;
 
-		events.add(new PointerEvent(key, state, x, y, amountX, amountY));
-	}
+        if (me.getClickCount() == 2) {
+            state = PointerState.DOUBLE_CLICK;
+        } else if (me.getClickCount() > 2) {
+            state = PointerState.MULTIPLE_CLICK;
+        }
 
-	@Override
-	public void mouseClicked( java.awt.event.MouseEvent me ) {
+        setCoordinates(me.getX(), me.getY());
+        addEvent(me.getButton(), state, me.getClickCount());
 
-		PointerState state = PointerState.CLICK;
+        me.consume();
+    }
 
-		if (me.getClickCount() == 2) {
-			state = PointerState.DOUBLE_CLICK;
-		}else if (me.getClickCount() > 2) {
-			state = PointerState.MULTIPLE_CLICK;
-		}
+    @Override
+    public void mousePressed(java.awt.event.MouseEvent me) {
 
-		setCoordinates(me.getX(),me.getY());
+        setCoordinates(me.getX(), me.getY());
 
-		addEvent(me.getButton(),state, me.getClickCount());
+        addEvent(me.getButton(), PointerState.PRESSED);
 
-		me.consume();
-	}
+        pressDragButton(me.getButton());
 
-	@Override
-	public void mousePressed( java.awt.event.MouseEvent me ) {
+        me.consume();
+    }
 
-		setCoordinates(me.getX(),me.getY());
+    private void pressDragButton(int button) {
+        DragEvent event = dragged.get(button);
+        event.active = true;
+    }
 
-		addEvent(me.getButton(),PointerState.PRESSED);
+    private void releaseDragButton(int button) {
+        DragEvent event = dragged.get(button);
+        event.active = false;
+    }
 
-		pressDragButton(me.getButton());
+    @Override
+    public void mouseReleased(java.awt.event.MouseEvent me) {
+        setCoordinates(me.getX(), me.getY());
 
-		me.consume();
-	}
+        addEvent(me.getButton(), PointerState.RELEASED);
 
-	private void pressDragButton(int button) {
+        releaseDragButton(me.getButton());
 
-		if(dragButton == 0) {
-			dragButton = button;			
-		}		
-	}
+        me.consume();
+    }
 
-	private void releaseDragButton(int button) {
+    @Override
+    public void mouseMoved(java.awt.event.MouseEvent me) {
+        setCoordinates(me.getX(), me.getY());
 
-		if(dragButton == button) {
-			dragged = false;
-			dragButton = 0;
-		}		
-	}
+        addMouseMoveEvent(x, y);
 
-	@Override
-	public void mouseReleased( java.awt.event.MouseEvent me ) {
+        me.consume();
+    }
 
-		setCoordinates(me.getX(),me.getY());
+    @Override
+    public void mouseEntered(java.awt.event.MouseEvent me) {
+        //mouseMoved( me );
+        me.consume();
+    }
 
-		addEvent(me.getButton(),PointerState.RELEASED);
+    @Override
+    public void mouseExited(java.awt.event.MouseEvent me) {
+        me.consume();
+    }
 
-		releaseDragButton(me.getButton());
+    @Override
+    public void mouseDragged(java.awt.event.MouseEvent me) {
+        DragEvent event = dragged.get(me.getButton());
+        event.x = me.getX();
+        event.y = me.getY();
 
-		me.consume();
-	}
+        int deltaX = me.getX() - event.x;
+        int deltaY = me.getY() - event.y;
 
-	@Override
-	public void mouseMoved( java.awt.event.MouseEvent me ) {
+        setCoordinates(me.getX(), me.getY());
 
-		setCoordinates(me.getX(),me.getY());
+        addEvent(me.getButton(), PointerState.DRAGGED, deltaX, deltaY);
+        me.consume();
+    }
 
-		addMouseMoveEvent(x, y);
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent mwe) {
+        MouseEvent key = MouseEvent.MOUSE_WHEEL_DOWN;
 
-		me.consume();
-	}
+        if (mwe.getWheelRotation() < 0) {
+            key = MouseEvent.MOUSE_WHEEL_UP;
+        }
 
-	@Override
-	public void mouseEntered( java.awt.event.MouseEvent me ) {
-		//mouseMoved( me );
-		me.consume();
-	}
+        events.add(new PointerEvent(key, PointerState.PRESSED, x, y, mwe.getWheelRotation()));
+    }
 
-	@Override
-	public void mouseExited( java.awt.event.MouseEvent me ) {
-		me.consume();
-	}
+    public void addMouseMoveEvent(int x, int y) {
+        events.add(new PointerEvent(MouseEvent.MOUSE_NONE, PointerState.MOVE, x, y));
+    }
 
-	@Override
-	public void mouseDragged( java.awt.event.MouseEvent me ) {
+    public Deque<PointerEvent> getEvents() {
+        return events;
+    }
 
-		if(!dragged) {
-			dragX = me.getX();
-			dragY = me.getY();
+    public void addEvent(PointerEvent event) {
+        events.add(event);
+    }
 
-			dragged = true;
-		}
+    public boolean hasEvents() {
+        return !events.isEmpty();
+    }
 
-		int deltaX = me.getX()-dragX;
-		int deltaY = me.getY()-dragY;
-
-		setCoordinates(me.getX(), me.getY());
-
-		addEvent(dragButton, PointerState.DRAGGED, deltaX, deltaY);
-
-		me.consume();		
-	}
-
-	@Override
-	public void mouseWheelMoved(MouseWheelEvent mwe) {
-
-		MouseEvent key = MouseEvent.MOUSE_WHEEL_DOWN;		
-
-		if(mwe.getWheelRotation()<0) {
-			key = MouseEvent.MOUSE_WHEEL_UP;
-		}
-
-		events.add(new PointerEvent(key, PointerState.PRESSED, x, y, mwe.getWheelRotation()));
-	}
-	
-	public void addMouseMoveEvent(int x, int y) {
-		events.add(new PointerEvent(MouseEvent.MOUSE_NONE, PointerState.MOVE, x, y));
-	}
-
-	public Deque<PointerEvent> getEvents() {
-		return events;
-	}
-
-	public void addEvent(PointerEvent event) {
-		events.add(event);
-	}
-
-	public boolean hasEvents() {
-		return !events.isEmpty();
-	}
-
+    /* package */ class DragEvent {
+        int x;
+        int y;
+        boolean active = false;
+    }
 }
---
+++ src/test/java/com/harium/etyl/core/input/mouse/MouseTest.java
@@ -0,0 +1,106 @@
+package com.harium.etyl.core.input.mouse;
+
+import com.harium.etyl.commons.event.MouseEvent;
+import com.harium.etyl.commons.event.PointerEvent;
+import com.harium.etyl.commons.event.PointerState;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class MouseTest {
+
+    Mouse mouse;
+
+    @Before
+    public void setUp() {
+        mouse = new Mouse(0,0);
+    }
+
+    @Test
+    public void testAddEvent() {
+        PointerEvent event1 = new PointerEvent(MouseEvent.MOUSE_NONE, PointerState.MOVE);
+        PointerEvent event2 = new PointerEvent(MouseEvent.MOUSE_BUTTON_LEFT, PointerState.CLICK);
+
+        mouse.addEvent(event1);
+        mouse.addEvent(event2);
+
+        Assert.assertEquals(2, mouse.getEvents().size());
+        Assert.assertEquals(event1, mouse.getEvents().pop());
+        Assert.assertEquals(event2, mouse.getEvents().pop());
+    }
+
+    @Test
+    public void testMouseMove() {
+        java.awt.event.MouseEvent event = mock(java.awt.event.MouseEvent.class);
+        doReturn(10).when(event).getX();
+        doReturn(20).when(event).getY();
+
+        mouse.mouseMoved(event);
+
+
+        verify(event, times(1)).getX();
+        verify(event, times(1)).getY();
+
+        Assert.assertEquals(1, mouse.getEvents().size());
+
+        PointerEvent generatedEvent = mouse.getEvents().pop();
+        Assert.assertEquals(10, generatedEvent.getX());
+        Assert.assertEquals(20, generatedEvent.getY());
+    }
+
+    @Test
+    public void testMouseLeftPressed() {
+        java.awt.event.MouseEvent event = mock(java.awt.event.MouseEvent.class);
+        doReturn(java.awt.event.MouseEvent.BUTTON1).when(event).getButton();
+        doReturn(10).when(event).getX();
+        doReturn(20).when(event).getY();
+
+        mouse.mousePressed(event);
+
+        verify(event, times(1)).getX();
+        verify(event, times(1)).getY();
+        Assert.assertEquals(1, mouse.getEvents().size());
+
+        PointerEvent generatedEvent = mouse.getEvents().pop();
+        Assert.assertEquals(MouseEvent.MOUSE_BUTTON_LEFT, generatedEvent.getKey());
+        Assert.assertEquals(10, generatedEvent.getX());
+        Assert.assertEquals(20, generatedEvent.getY());
+
+        Mouse.DragEvent dragEvent = mouse.dragged.get(java.awt.event.MouseEvent.BUTTON1);
+        Assert.assertEquals(true, dragEvent.active);
+    }
+
+    @Test
+    public void testMouseLeftReleased() {
+        java.awt.event.MouseEvent event = mock(java.awt.event.MouseEvent.class);
+        doReturn(java.awt.event.MouseEvent.BUTTON1).when(event).getButton();
+        doReturn(10).when(event).getX();
+        doReturn(20).when(event).getY();
+
+        mouse.mouseReleased(event);
+
+        verify(event, times(1)).getX();
+        verify(event, times(1)).getY();
+        Assert.assertEquals(1, mouse.getEvents().size());
+
+        PointerEvent generatedEvent = mouse.getEvents().pop();
+        Assert.assertEquals(MouseEvent.MOUSE_BUTTON_LEFT, generatedEvent.getKey());
+        Assert.assertEquals(10, generatedEvent.getX());
+        Assert.assertEquals(20, generatedEvent.getY());
+
+        Mouse.DragEvent dragEvent = mouse.dragged.get(java.awt.event.MouseEvent.BUTTON1);
+        Assert.assertEquals(false, dragEvent.active);
+    }
+
+    @Test
+    public void testInit() {
+        Assert.assertEquals(3, mouse.dragged.size());
+        Assert.assertTrue(mouse.dragged.containsKey(java.awt.event.MouseEvent.BUTTON1));
+        Assert.assertTrue(mouse.dragged.containsKey(java.awt.event.MouseEvent.BUTTON2));
+        Assert.assertTrue(mouse.dragged.containsKey(java.awt.event.MouseEvent.BUTTON3));
+    }
+
+
+}
