a2b25e67ca8a5364c5c425a6f49752a9e04c0766
Yuri Pourre <yuripourre@gmail.com>
2018-07-24 11:19:42 -0300

    Add font constants to Font

--- src/main/java/com/harium/etyl/core/graphics/Font.java
+++ src/main/java/com/harium/etyl/core/graphics/Font.java
@@ -1,7 +1,29 @@
 package com.harium.etyl.core.graphics;
 
+/**
+ * Font class to wrap native fonts
+ * Constants borrowed from awt
+ */
 public class Font {
-    java.awt.Font font;
+
+    /**
+     * The plain style constant.
+     */
+    public static final int PLAIN = 0;
+
+    /**
+     * The bold style constant.  This can be combined with the other style
+     * constants (except PLAIN) for mixed styles.
+     */
+    public static final int BOLD = 1;
+
+    /**
+     * The italicized style constant.  This can be combined with the other
+     * style constants (except PLAIN) for mixed styles.
+     */
+    public static final int ITALIC = 2;
+
+    protected java.awt.Font font;
 
     public Font(java.awt.Font font) {
         this.font = font;
