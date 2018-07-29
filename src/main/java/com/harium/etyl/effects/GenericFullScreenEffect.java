aaf059bf002d5782c0e1cbd8fd5c23fc3ae7ad1d
Yuri Pourre <yuripourre@gmail.com>
2018-07-29 11:22:06 -0300

    Refactoring

--- src/main/java/com/harium/etyl/effects/GenericFullScreenEffect.java
+++ src/main/java/com/harium/etyl/effects/GenericFullScreenEffect.java
@@ -1,59 +1,55 @@
 package com.harium.etyl.effects;
 
-import java.awt.Color;
-
+import com.harium.etyl.commons.graphics.Color;
 import com.harium.etyl.core.animation.script.OpacityAnimation;
 import com.harium.etyl.core.effect.GlobalEffect;
 import com.harium.etyl.core.graphics.Graphics;
 import com.harium.etyl.i18n.DefaultDictionary;
+import com.harium.etyl.i18n.Dictionary;
 import com.harium.etyl.i18n.LanguageModule;
 
 /**
- * 
  * @author yuripourre
- *
  */
 
 public class GenericFullScreenEffect extends GlobalEffect {
-	
-	private DefaultDictionary dictionary;
-		
-	public GenericFullScreenEffect(int x, int y, int w, int h) {
-		super(x, y, w, h);
-			
-		dictionary = new DefaultDictionary();
-		
-		//3 seconds animation
-		script = new OpacityAnimation(this, 3000);
-		script.setInterval(255, 0);
-		
-	}
-	
-	private final float rectW = 360;
-	private final float rectH = 50;
-	private final float rectX = w/2-rectW/2;
-	private final float rectY = h/2-h/4;
-	
-	@Override
-	public void draw(Graphics g) {
-		
-		g.setOpacity(opacity);
-				
-		g.setColor(Color.BLACK);
-		g.fillArc(rectX-rectH/2, rectY, rectH, rectH, 90, 180);
-		g.fillRect(rectX, rectY, rectW, rectH);
-		g.fillArc(rectX+rectW-rectH/2, rectY, rectH, rectH, 270, 180);
-		
-		g.setColor(Color.WHITE);
-		g.setFont(g.getFont().deriveFont(20f));
-		
-		String sentence = dictionary.getText(LanguageModule.getInstance().getLanguage(), DefaultDictionary.MESSAGE_FULLSCREEN);
-		
-		g.drawStringShadow(sentence, rectX, rectY, rectW, rectH, Color.BLACK);
-		
-		g.setOpacity(255);
-				
-	}
-		
+
+    private Dictionary dictionary;
+
+    private static final int RECT_W = 360;
+    private static final int RECT_H = 50;
+    private final int RECT_X = w / 2 - RECT_W / 2;
+    private final int RECT_Y = h / 2 - h / 4;
+
+    public GenericFullScreenEffect(int x, int y, int w, int h) {
+        super(x, y, w, h);
+
+        dictionary = DefaultDictionary.getInstance();
+
+        // 3 seconds of animation
+        script = new OpacityAnimation(this, 3000);
+        script.setInterval(255, 0);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+        g.setOpacity(opacity);
+
+        g.setColor(Color.BLACK);
+        g.fillArc(RECT_X - RECT_H / 2, RECT_Y, RECT_H, RECT_H, 90, 180);
+        g.fillRect(RECT_X, RECT_Y, RECT_W, RECT_H);
+        g.fillArc(RECT_X + RECT_W - RECT_H / 2, RECT_Y, RECT_H, RECT_H, 270, 180);
+
+        g.setColor(Color.WHITE);
+        g.setFont(g.getFont().deriveFont(20f));
+
+        String sentence = dictionary.getText(LanguageModule.getInstance().getLanguage(), DefaultDictionary.MESSAGE_FULLSCREEN);
+
+        g.drawStringShadow(sentence, RECT_X, RECT_Y, RECT_W, RECT_H, Color.BLACK);
+
+        g.setOpacity(255);
+    }
+
 }
 
--- src/main/java/com/harium/etyl/i18n/DefaultDictionary.java
+++ src/main/java/com/harium/etyl/i18n/DefaultDictionary.java
@@ -3,25 +3,31 @@ package com.harium.etyl.i18n;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DefaultDictionary extends Dictionary {
-	
-	public static final String MESSAGE_FULLSCREEN = "fullscreen";
-	
-	public DefaultDictionary() {
-		super();
-		defaultLanguage = Language.ENGLISH_US;
-		populateVocabulary();
-	}
-	
-	private void populateVocabulary() {
-		Map<String,String> usVocabulary = new HashMap<String,String>();
-		usVocabulary.put(MESSAGE_FULLSCREEN, "Press ESC to exit fullscreen.");
-		
-		Map<String,String> brVocabulary = new HashMap<String,String>();
-		brVocabulary.put(MESSAGE_FULLSCREEN, "Pressione ESC para sair da tela cheia.");
-		
-		sentences.put(Language.ENGLISH_US, usVocabulary);
-		sentences.put(Language.PORTUGUESE_BRAZIL, brVocabulary);
-	}
+public class DefaultDictionary {
+
+    private static Dictionary dictionary;
+
+    public static final String MESSAGE_FULLSCREEN = "fullscreen";
+
+    public static Dictionary getInstance() {
+        if (dictionary == null) {
+            dictionary = new Dictionary();
+            dictionary.setDefaultLanguage(Language.ENGLISH_US);
+
+            populateVocabulary(dictionary);
+        }
+        return dictionary;
+    }
+
+    private static void populateVocabulary(Dictionary dictionary) {
+        Map<String, String> usVocabulary = new HashMap<String, String>();
+        usVocabulary.put(MESSAGE_FULLSCREEN, "Press ESC to exit fullscreen.");
+
+        Map<String, String> brVocabulary = new HashMap<String, String>();
+        brVocabulary.put(MESSAGE_FULLSCREEN, "Pressione ESC para sair da tela cheia.");
+
+        dictionary.sentences.put(Language.ENGLISH_US, usVocabulary);
+        dictionary.sentences.put(Language.PORTUGUESE_BRAZIL, brVocabulary);
+    }
 
 }
